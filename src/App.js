3ad6c6ab05ec3506c0fdb294689419032e1151ec
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-01-24 07:18:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path={'/'}
+          element={
+            // <PrivateRoute>
+              <Dashboard />
+            // </PrivateRoute>
+          }
+        />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
