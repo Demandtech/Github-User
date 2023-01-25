93313f8477a3a92b1e1af6cdf88d13dc6e51d67c
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-01-25 06:27:07 +0100

    feat: piechart implementation

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from 'react'
+import styled from 'styled-components'
+import { GithubContext } from '../context/context'
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts'
 const Repos = () => {
-  return <h2>repos component</h2>;
-};
+  const { repos } = React.useContext(GithubContext)
+  
+  let languages = repos.reduce((total, item)=> {
+   const {language} = item
+   if(!language) return total
+   if(!total[language]) {
+    total[language] = 1
+   }else{
+    total[language] = total[language] + 1
+   }
+   total[language] = 30
+   return total
+  })
+
+  const data = [
+    { label: 'Html', value: '12' },
+    { label: 'css', value: '15' },
+    { label: 'js', value: '13' },
+    { label: 'react', value: '24' },
+    
+  ]
+  return (
+    <section className='section'>
+      <Wrapper>
+        {/* <ExampleChart  data={data}/> */}
+        <Pie3D data={data}/>
+      </Wrapper>
+    </section>
+  )
+}
 
 const Wrapper = styled.div`
   display: grid;
@@ -28,6 +56,6 @@ const Wrapper = styled.div`
     width: 100% !important;
     border-radius: var(--radius) !important;
   }
-`;
+`
 
-export default Repos;
+export default Repos
